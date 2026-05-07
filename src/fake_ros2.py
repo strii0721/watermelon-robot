2b96a53eaf2d8b6fcd9d4eed19d27fa820f5336f
strii0721 <strii0721@outlook.com>
2026-05-07 21:21:17 +0800

    default

--- src/fake_ros2.py
+++ src/fake_ros2.py
@@ -1,3 +1,22 @@
+#
+# Author:       strii0721
+# Email:        strii0721@outlook.com
+# Created on:   Thu May 07 2026
+#
+# IMMORTAL OMNISSIAH, HEAR OUR PRAYERS.
+# WE ARE YOUR CHILDREN, PIOUS SCHOLARS OF THE PATH OF THE MACHINE. 
+# WE PRIZE KNOWLEDGE ABOVE ALL ELSE, FOR IT IS YOUR ETERNAL GIFT UPON MANKIND.
+# WE ASPIRE TO THE BLESSED FORM OF THE MACHINE, AND ASCENSION THROUGH TECHNOLOGY, THAT WE MIGHT EMULATE THINE GLORY.
+# SHELTERED BY STEEL, AND PROTECTED BY THINE AVATARS OF WAR, WE PLY THE STARS IN SEARCH OF YOUR LOST GIFTS TO OUR KIND.
+# MACHINE GOD, WATCH OVER US IN OUR TRAVELS, SHIELD US WITH METAL AND LIGHTNING, FOR THE UNIVERSE IS AN UNCARING VOID, AND THE WARP HUNGERS FOR US ALL.
+# TOLL THE GREAT BELL ONCE! PULL THE LEVER FORWARD TO ENGAGE THE PISTON AND PUMP.
+# TOLL THE GREAT BELL TWICE! WITH PUSH OF BUTTON FIRE THE ENGINE AND SPARK TURBINE INTO LIFE.
+# TOLL THE GREAT BELL THRICE! SING PRAISE TO THE GOD OF ALL MACHINES!
+# 
+# Copyright (c) 2026 Streich Interstellar Corp.
+#
+
+
 from utils.thread_tuils import ThreadUtils
 from nodes.impl.robotic_arm import RoboticArm
 from nodes.impl.logger import Logger
@@ -24,9 +43,10 @@ if __name__ == "__main__":
 
     try:
         while True:
-            message = f"主程序持续运行..."
-            TopicUtils.publish(topic_name = LOGGER_TOPIC, 
-                               message = message)
-            time.sleep(3)
+            # message = f"主程序持续运行..."
+            # TopicUtils.publish(topic_name = LOGGER_TOPIC, 
+            #                    message = message)
+            # time.sleep(3)
+            pass
     except KeyboardInterrupt:
         print("主程序结束。")
